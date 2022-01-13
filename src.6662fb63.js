b6a6369c8283b3bba720cfe1494ca03b85c95e52
HalynaStep <94197972+HalynaStep@users.noreply.github.com>
2022-01-13 18:42:02 +0000

    Deploying to gh-pages from @ asst199162/project2022-icecream@2775296a3918c7128ebf1767a69b6c818a6821ca 🚀

--- src.6662fb63.js
+++ src.6662fb63.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/project2022-icecream/src.7abcbfbd.js.map
+//# sourceMappingURL=/project2022-icecream/src.6662fb63.js.map
