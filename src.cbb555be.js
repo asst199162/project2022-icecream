990af6fc0ed1aec4d85921a3dcc3685bbd28b6e4
asst199162 <42334307+asst199162@users.noreply.github.com>
2022-01-14 15:13:22 +0000

    Deploying to gh-pages from @ asst199162/project2022-icecream@ac2115f87433a8531bc3e4c124ca5ccc879f2f79 🚀

--- src.cbb555be.js
+++ src.cbb555be.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/project2022-icecream/src.7f1030a5.js.map
+//# sourceMappingURL=/project2022-icecream/src.cbb555be.js.map
